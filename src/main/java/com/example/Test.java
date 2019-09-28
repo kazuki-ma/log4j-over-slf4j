349f56d5f23d59bbb53b2a4ed207c866122b5ad5
Kazuki MATSUDA / 松田一樹 <matsuda.kazuki@gmail.com>
2019-09-28 23:52:03 +0900

    Add spring-boot sample.

--- src/main/java/com/example/Test.java
+++
@@ -1,6 +0,0 @@
-package com.example;
-
-class Test {
-    
-}
-
--- test-spring/src/test/java/com/example/LoggingTest.java
+++ test-spring/src/test/java/com/example/LoggingTest.java
@@ -1,14 +1,13 @@
 package com.example;
 
 import org.junit.Test;
-import org.slf4j.bridge.SLF4JBridgeHandler;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = LoggingTest.class)
 public class LoggingTest {
-    static {
-        SLF4JBridgeHandler.removeHandlersForRootLogger();
-        SLF4JBridgeHandler.install();
-    }
-
     @Test
     public void logging() {
         new JDK().run();
